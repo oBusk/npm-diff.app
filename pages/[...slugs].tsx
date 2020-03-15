d60dd7bfd383b94b99551a6745b0a269832fb9ec
Oscar Busk <oscar.busk@gmail.com>
2020-03-15 22:36:38 +0100

    feat: add stupid redirection

--- pages/[...slugs].tsx
+++ pages/[...slugs].tsx
@@ -3,13 +3,13 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import fetch from "isomorphic-unfetch";
-import * as React from "react";
-import { Diff, Hunk, parseDiff, DiffFile } from "react-diff-view";
 import { diffLines } from "diff";
-import Layout from "../components/Layout";
-import { formatLines } from "unidiff";
+import unfetch from "isomorphic-unfetch";
 import { NextPageContext } from "next";
+import * as React from "react";
+import { Diff, DiffFile, Hunk, parseDiff } from "react-diff-view";
+import { formatLines } from "unidiff";
+import Layout from "../components/Layout";
 
 type Props = {
     packageA?: string;
@@ -18,19 +18,64 @@ type Props = {
     errors?: string;
 };
 
+async function getRedirectOrMeta(
+    packageQuery: string,
+): Promise<{ status: 302; pkgName: string } | { status: number; json: any }> {
+    const url = `https://unpkg.com/${packageQuery}/?meta`;
+    const response = await unfetch(url, { redirect: "manual" });
+    console.log(response.status);
+    if (response.status === 302) {
+        const location = response.headers.get("location")!;
+        const pkgName = /^https:\/\/unpkg\.com\/(.*)\/\?meta$/.exec(
+            location,
+        )![1];
+
+        return {
+            status: 302,
+            pkgName,
+        };
+    } else {
+        const json = await response.json();
+        return { status: response.status, json };
+    }
+}
+
 class DiffPage extends React.Component<Props> {
-    static getInitialProps = async ({ query }: NextPageContext) => {
+    static getInitialProps = async ({ query, res }: NextPageContext) => {
         try {
             const { slugs } = query;
             const slug = Array.isArray(slugs) ? slugs.join("/") : slugs;
             const [packageA, packageB] = slug.split("...");
+            const [metaOrRedirectA, metaOrRedirectB] = await Promise.all([
+                getRedirectOrMeta(packageA),
+                getRedirectOrMeta(packageB),
+            ]);
+
+            if (
+                metaOrRedirectA.status === 302 ||
+                metaOrRedirectB.status === 302
+            ) {
+                const newPackageA =
+                    (metaOrRedirectA as any).pkgName ?? packageA;
+                const newPackageB =
+                    (metaOrRedirectB as any).pkgName ?? packageB;
+
+                // https://spectrum.chat/next-js/general/high-level-301-redirects~669a5349-81c5-441f-a2d8-69d79d5815a8?m=MTU3OTcxNDY3NTk0Mg==
+                // use 302 because that's what unpkg does
+                res?.writeHead(302, {
+                    Location: `${newPackageA}...${newPackageB}`,
+                });
+                res?.end();
+                return {};
+            }
+
             const [aFile, bFile] = await Promise.all([
-                fetch(`https://unpkg.com/${packageA}/package.json`).then(res =>
-                    res.text(),
-                ),
-                fetch(`https://unpkg.com/${packageB}/package.json`).then(res =>
-                    res.text(),
-                ),
+                unfetch(
+                    `https://unpkg.com/${packageA}/package.json`,
+                ).then(res => res.text()),
+                unfetch(
+                    `https://unpkg.com/${packageB}/package.json`,
+                ).then(res => res.text()),
             ]);
 
             const changes = diffLines(aFile, bFile);
