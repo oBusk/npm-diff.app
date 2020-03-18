f29e049f015f8182a38fbdc0e65454361baea25c
Oscar Busk <oscar.busk@gmail.com>
2020-03-18 23:11:30 +0100

    feat: show very simple diffs 📦🔃

--- src/pages/[...slugs].tsx
+++ src/pages/[...slugs].tsx
@@ -3,10 +3,21 @@ import * as React from "react";
 import { getPkgDetails } from "../util/getPkgDetails";
 import { parsePackageString } from "../util/npm-parser";
 import { fetchTarBall } from "../util/npm-api";
+import { getDiff } from "../util/getDiff";
+import Layout from "../components/Layout";
+import { Diff, DiffFile, Hunk, parseDiff } from "react-diff-view";
 
 type Props = {
-    output: any;
+    diff: string;
 };
+
+function getPackageStrings(queries: string | string[]): [string, string] {
+    const query = typeof queries === "string" ? queries : queries[0];
+
+    const [pkg1, pkg2] = query.split("...");
+
+    return [pkg1, pkg2];
+}
 
 class DiffPage extends React.Component<Props> {
     static getInitialProps = async ({
@@ -14,20 +25,67 @@ class DiffPage extends React.Component<Props> {
     }: NextPageContext): Promise<Props> => {
         const { slugs } = query;
 
-        const { name, versionOrTag } = parsePackageString(
-            Array.isArray(slugs) ? slugs[0] : slugs,
-        );
+        const [p1, p2] = getPackageStrings(slugs);
 
-        const { version, tarballUrl } = await getPkgDetails(name, versionOrTag);
+        const p1StringParse = parsePackageString(p1);
+        const p2StringParse = parsePackageString(p2);
 
-        const files = await fetchTarBall(tarballUrl);
+        const [p1Details, p2Details] = await Promise.all([
+            getPkgDetails(p1StringParse.name, p1StringParse.versionOrTag),
+            getPkgDetails(p2StringParse.name, p2StringParse.versionOrTag),
+        ]);
 
-        return { output: { files, version } };
+        const [p1Files, p2Files] = await Promise.all([
+            fetchTarBall(p1Details.tarballUrl),
+            fetchTarBall(p2Details.tarballUrl),
+        ]);
+
+        const p1Result = {
+            files: p1Files,
+            version: p1Details.version,
+        };
+        const p2Result = {
+            files: p2Files,
+            version: p2Details.version,
+        };
+
+        const diff = getDiff(p1Result.files, p2Result.files);
+
+        return { diff };
     };
 
     render(): JSX.Element {
-        const { output } = this.props;
-        return <pre>{JSON.stringify(output, null, 4)}</pre>;
+        const { diff } = this.props;
+
+        const files = parseDiff(diff);
+
+        const renderFile = ({
+            oldRevision,
+            newRevision,
+            type,
+            hunks,
+        }: DiffFile): JSX.Element => {
+            return (
+                <Diff
+                    key={oldRevision + "-" + newRevision}
+                    viewType="split"
+                    diffType={type}
+                    hunks={hunks}
+                >
+                    {(hunks: any): JSX.Element =>
+                        hunks.map((hunk: any) => (
+                            <Hunk key={hunk.content} hunk={hunk} />
+                        ))
+                    }
+                </Diff>
+            );
+        };
+
+        return (
+            <Layout>
+                <div>{files.map(renderFile)}</div>
+            </Layout>
+        );
     }
 }
 
---
+++ src/util/getDiff.ts
@@ -0,0 +1,24 @@
+import { diffLines } from "diff";
+import { formatLines } from "unidiff";
+
+export function getDiff(
+    filesA: { [k: string]: string },
+    filesB: { [k: string]: string },
+): string {
+    let diff = "";
+
+    Object.entries(filesB).forEach(([fileName, contentB]) => {
+        const contentA = filesA[fileName] || "";
+
+        const changes = diffLines(contentA, contentB);
+        const fileDiff = formatLines(changes, {
+            aname: fileName,
+            bname: fileName,
+            context: Number.MAX_SAFE_INTEGER,
+        });
+
+        diff += fileDiff;
+    });
+
+    return diff;
+}
