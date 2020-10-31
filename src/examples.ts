3a4f83a258dce5bd94af1ba77613cca6855c91d4
Oscar Busk <oscar.busk@gmail.com>
2020-10-31 03:03:53 +0100

    refactor: Freeze example array

--- src/examples.ts
+++ src/examples.ts
@@ -1,5 +1,5 @@
-export const EXAMPLES = [
+export const EXAMPLES = Object.freeze([
     "lodash@4.17.0...lodash@4.17.19",
     "chalk@4.0.0...chalk@4.1.0",
     "tslib@1.13.0...tslib@2.0.0",
-];
+]);
