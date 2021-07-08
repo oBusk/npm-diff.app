f465382ebab5bce21f1dae9d863daf5752c87133
Oscar Busk <oscar.busk@gmail.com>
2021-07-08 23:41:01 +0200

    Update about/api

--- src/examples.ts
+++ src/examples.ts
@@ -1,5 +1,6 @@
 export const EXAMPLES = Object.freeze([
-    "lodash@4.17.0...lodash@4.17.19",
-    "chalk@4.0.0...chalk@4.1.0",
+    "@types/node@15.12.1...@types/node@15.12.2",
+    "lodash@4.17.0...4.17.19",
+    "4.0.0...chalk@4.1.0",
     "tslib@1.13.0...tslib@2.0.0",
 ]);
--- src/pages/about/api.tsx
+++ src/pages/about/api.tsx
@@ -1,46 +1,61 @@
-import { Code, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { Code, Heading, Link, Text, VStack } from "@chakra-ui/react";
 import { withTheme } from "@emotion/react";
 import Layout from "components/Layout";
 import { EXAMPLES } from "examples";
-import { NextPage } from "next";
+import { npmDiff } from "lib/npm-diff";
+import { partsToSpecs } from "lib/parts-to-specs";
+import { GetStaticProps, NextPage } from "next";
 
+// TODO: Would be nice if this was dynamic. But doesn't seem possible
+// https://github.com/vercel/next.js/discussions/12848
 const BASE_PATH = "https://npm-diff.app";
 const API_PATH = `${BASE_PATH}/api`;
 const EXAMPLE_QUERY = EXAMPLES[0];
 const EXAMPLE_URL = `${API_PATH}/${EXAMPLE_QUERY}`;
 
-const EXAMPLE_DIFF = `
---- package/LICENSE
-+++ package/LICENSE
-@@ -1,4 +1,4 @@
--Copyright JS Foundation and other contributors <https://js.foundation/>
-+Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
+type Props = {
+    diff: string;
+    specs: [string, string];
+};
 
- Based on Underscore.js, copyright Jeremy Ashkenas,
- DocumentCloud and Investigative Reporters & Editors <http://underscorejs.org/>--- package/_baseClone.js
- `;
+export const getStaticProps: GetStaticProps<Props> = async ({}) => {
+    const specs = partsToSpecs(EXAMPLE_QUERY);
+    const diff = await npmDiff(specs);
 
-const ApiPage: NextPage<{}> = () => {
+    return { props: { diff, specs } };
+};
+
+const ApiPage: NextPage<Props> = ({ diff, specs }) => {
     return (
         <Layout>
-            <Stack p={5} shadow="md" borderWidth="1px" spacing={8}>
+            <VStack p={5} shadow="md" borderWidth="1px" spacing={8}>
                 <Heading as="h2" size="lg">
                     npm-diff.app API
                 </Heading>
                 <Text>
-                    There is a very simple API to get the difference between two
-                    versions of a package
+                    npm-diff.app exposes a online API to equal{" "}
+                    <Code>npm diff</Code>, to be able to see the changes between
+                    versions of packages or forks of packages.
                 </Text>
 
-                <Code>
-                    GET <Link href={EXAMPLE_URL}>{EXAMPLE_URL}</Link>
-                </Code>
                 <Text>
-                    will return a <Text as="i">diff</Text> of the two provided
-                    packages
+                    <Code>
+                        GET <Link href={EXAMPLE_URL}>{EXAMPLE_URL}</Link>
+                    </Code>
+                    <br />
+                    will return the same as
+                    <br />
+                    <Code>
+                        npm diff --diff={specs[0]} --diff={specs[1]}
+                    </Code>
                 </Text>
-                <Code as="pre">{EXAMPLE_DIFF}</Code>
-            </Stack>
+                <Text>
+                    a <Text as="i">diff</Text> of the two provided packages
+                </Text>
+                <Code as="pre" whiteSpace="pre-wrap">
+                    {diff}
+                </Code>
+            </VStack>
         </Layout>
     );
 };
