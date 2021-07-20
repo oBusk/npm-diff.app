8bb05c08c7280dd194d80daa3a5bb6872798e17c
Oscar Busk <oscar.busk@gmail.com>
2021-07-20 23:27:08 +0200

    docs: Document what `convertVersionsToSpecs` does

--- src/lib/get-absolute-specs.ts
+++ src/lib/get-absolute-specs.ts
@@ -3,8 +3,16 @@ import semver from "semver";
 import pacote from "pacote";
 
 /**
- * Converts two inputs matching what `npm diff --diff=` expects and returns
- * specs such as `package-name@version`.
+ * `npm diff` supports taking both `--diff` inputs as versions and/or semver
+ * which then defaults to the current package.json name. It also supports
+ * one of the `--diff` to be only semver/version, and then uses the package
+ * name from the other input.
+ *
+ * We don't care about the "current package.json" usecase, but we want to
+ * support specifying only the version in one of the inputs.
+ *
+ * This does the version-handling that `npm/cli` does, but removing alot of
+ * fallbacks to local directory.
  *
  * > Based on: https://github.com/npm/cli/blob/v7.19.1/lib/diff.js#L235-L264
  */
@@ -12,7 +20,7 @@ function convertVersionsToSpecs([a, b]: [string, string]): [string, string] {
     const semverA = semver.validRange(a);
     const semverB = semver.validRange(b);
 
-    // both specs are semver versions, assume current project dir name
+    // We cannot support both args being only versions, we need a package name
     if (semverA && semverB) {
         throw new Error("One of the specs must contain package name");
     }
