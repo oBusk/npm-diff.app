9f12d3c3bf6e2a181f0185aed0b024500c1f1cdc
Oscar Busk <oscar.busk@gmail.com>
2021-07-27 12:14:40 +0200

    perf(cache): 📚 Only cache on client in 5 minutes

--- src/lib/utils/setCacheControl.ts
+++ src/lib/utils/setCacheControl.ts
@@ -4,7 +4,13 @@ const minute = 60;
 const hour = 60 * minute;
 const day = 24 * hour;
 
-const cacheControl = `public, max-age=${day * 31}`;
+const cacheControl = [
+    "public",
+    // Let's cache 5 minutes on client side
+    `max-age=${5 * minute}`,
+    // And the maximum supported vercel 31 days
+    `s-maxage=${31 * day}`,
+].join(", ");
 
 function setCacheControl(res: ServerResponse): void {
     res.setHeader("Cache-Control", cacheControl);
