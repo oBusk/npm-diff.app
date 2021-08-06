f541b07d17f9fa5712bdf970ca31d0d8f29600d0
Oscar Busk <oscar.busk@gmail.com>
2021-08-06 02:07:38 +0200

    feat(ui): Test styling combobox with chakra

--- src/pages/autocomplete.tsx
+++ src/pages/autocomplete.tsx
@@ -1,3 +1,14 @@
+import { ArrowDownIcon } from "@chakra-ui/icons";
+import {
+    Button,
+    FormLabel,
+    IconButton,
+    Input,
+    InputGroup,
+    InputRightElement,
+} from "@chakra-ui/react";
+import Layout from "components/Layout";
+import BorderBox from "components/theme/BorderBox";
 import { useCombobox } from "downshift";
 import { NextPage } from "next";
 import { useState } from "react";
@@ -29,35 +40,41 @@ const AutocompletePage: NextPage<AutocompletePageProps> = (props, context) => {
     });
 
     return (
-        <>
-            <label {...getLabelProps()}>Choose an element:</label>
-            <div style={{}} {...getComboboxProps()}>
-                <input {...getInputProps()} />
-                <button
-                    type="button"
-                    {...getToggleButtonProps()}
-                    aria-label="toggle-menu"
-                >
-                    &#8595;
-                </button>
-            </div>
-            <ul {...getMenuProps()} style={{}}>
-                {isOpen &&
-                    inputItems.map((item, index) => (
-                        <li
-                            style={
-                                highlightedIndex === index
-                                    ? { backgroundColor: "#bde4ff" }
-                                    : {}
-                            }
-                            key={`${item}${index}`}
-                            {...getItemProps({ item, index })}
+        <Layout alignItems="center">
+            <BorderBox>
+                <FormLabel {...getLabelProps()}>Choose an element:</FormLabel>
+                <InputGroup size="md" style={{}} {...getComboboxProps()}>
+                    <Input {...getInputProps()} />
+                    <InputRightElement>
+                        <IconButton
+                            type="button"
+                            size="sm"
+                            icon={<ArrowDownIcon />}
+                            {...getToggleButtonProps()}
+                            aria-label="toggle-menu"
                         >
-                            {item}
-                        </li>
-                    ))}
-            </ul>
-        </>
+                            &#8595;
+                        </IconButton>
+                    </InputRightElement>
+                </InputGroup>
+                <ul {...getMenuProps()} style={{}}>
+                    {isOpen &&
+                        inputItems.map((item, index) => (
+                            <li
+                                style={
+                                    highlightedIndex === index
+                                        ? { backgroundColor: "#bde4ff" }
+                                        : {}
+                                }
+                                key={`${item}${index}`}
+                                {...getItemProps({ item, index })}
+                            >
+                                {item}
+                            </li>
+                        ))}
+                </ul>
+            </BorderBox>
+        </Layout>
     );
 };
 
