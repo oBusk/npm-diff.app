c6a085979300c179c6d2ba8c9fdcc15ef6432b0c
Oscar Busk <oscar.busk@gmail.com>
2021-08-06 19:33:48 +0200

    feat(ui): Add typing and initial packages to show

--- src/pages/autocomplete.tsx
+++ src/pages/autocomplete.tsx
@@ -11,36 +11,140 @@ import {
 import Layout from "components/Layout";
 import BorderBox from "components/theme/BorderBox";
 import { useCombobox } from "downshift";
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { useState } from "react";
 
-function getSuggestions(query: string) {
-    const suggestionSort = (packageA: any, packageB: any) => {
+export interface NpmsPackageLinks {
+    npm: string;
+    homepage: string;
+    repository: string;
+    bugs: string;
+}
+
+export interface NpmsPackageAuthor {
+    name: string;
+}
+
+export interface NpmsPackageMaintainer {
+    username: string;
+    email: string;
+}
+
+export interface NpmsPackage {
+    name: string;
+    scope: string;
+    version: string;
+    description: string;
+    keywords: string[];
+    date: string;
+    links: NpmsPackageLinks;
+    author: NpmsPackageAuthor;
+    publisher: NpmsPackageMaintainer;
+    maintainers: NpmsPackageMaintainer[];
+}
+
+export interface NpmsFlags {
+    deprecated?: true;
+    unstable?: true;
+    insecure?: true;
+}
+
+export interface NpmsSuggestionScoreDetail {
+    quality: number;
+    popularity: number;
+    maintenance: number;
+}
+
+export interface NpmsSuggestionScore {
+    final: number;
+    detail: NpmsSuggestionScoreDetail;
+}
+
+// https://api-docs.npms.io/
+export interface NpmsSuggestion extends NpmsSearchResult {
+    /** A string containing highlighted matched text */
+    highlight?: string;
+}
+
+export interface NpmsSearchResult {
+    /** The package data which contains the name, version and other useful information */
+    package: NpmsPackage;
+    /** The package flags (deprecated, unstable, insecure) */
+    flags?: NpmsFlags;
+    /** The package score */
+    score: NpmsSuggestionScore;
+    /** The computed search score (from Elasticsearch) */
+    searchScore: number;
+}
+
+export type NpmsSuggestions = NpmsSuggestion[];
+
+export interface NpmsSearchResults {
+    total: number;
+    results: NpmsSearchResult[];
+}
+
+async function getSuggestions(query: string): Promise<string[]> {
+    const suggestionSort = (
+        suggestionA: NpmsSuggestion,
+        suggestionB: NpmsSuggestion,
+    ) => {
         // Rank closely matching packages followed
         // by most popular ones
         if (
             Math.abs(
-                Math.log(packageB.searchScore) - Math.log(packageA.searchScore),
+                Math.log(suggestionB.searchScore) -
+                    Math.log(suggestionA.searchScore),
             ) > 1
         ) {
-            return packageB.searchScore - packageA.searchScore;
+            return suggestionB.searchScore - suggestionA.searchScore;
         } else {
             return (
-                packageB.score.detail.popularity -
-                packageA.score.detail.popularity
+                suggestionB.score.detail.popularity -
+                suggestionA.score.detail.popularity
             );
         }
     };
 
-    return fetch(`https://api.npms.io/v2/search/suggestions?q=${query}`)
-        .then((result) => result.json())
-        .then((result) => result.sort(suggestionSort));
+    const response = await fetch(
+        `https://api.npms.io/v2/search/suggestions?q=${query}`,
+    );
+    const json: NpmsSuggestions = await response.json();
+    const sorted = json.sort(suggestionSort);
+    const packageNames = sorted.map((suggestion) => suggestion.package.name);
+
+    return packageNames;
 }
 
-export interface AutocompletePageProps {}
+async function getPopularPackages(): Promise<NpmsSearchResults> {
+    const response = await fetch(
+        `https://api.npms.io/v2/search?q=not:deprecated`,
+    );
+    const json = await response.json();
+    return json;
+}
 
-const AutocompletePage: NextPage<AutocompletePageProps> = (props, context) => {
-    const [inputItems, setInputItems] = useState([]);
+export interface AutocompletePageProps {
+    popularPackages: string[];
+}
+
+export const getStaticProps: GetStaticProps<AutocompletePageProps> =
+    async () => {
+        const { results } = await getPopularPackages();
+        const popularPackages = results.map((p) => p.package.name);
+
+        return {
+            props: {
+                popularPackages,
+            },
+            revalidate: 60 * 60,
+        };
+    };
+
+const AutocompletePage: NextPage<AutocompletePageProps> = ({
+    popularPackages,
+}) => {
+    const [inputItems, setInputItems] = useState(popularPackages);
     const {
         getComboboxProps,
         getInputProps,
@@ -52,13 +156,13 @@ const AutocompletePage: NextPage<AutocompletePageProps> = (props, context) => {
         isOpen,
     } = useCombobox({
         items: inputItems,
-        onInputValueChange: ({ inputValue = "" }) => {
-            getSuggestions(inputValue).then((suggestions) => {
-                // console.log(suggestions);
-                setInputItems(
-                    suggestions.map((result: any) => result.package.name),
-                );
-            });
+        initialIsOpen: true,
+        onInputValueChange: async ({ inputValue = "" }) => {
+            const packages =
+                inputValue.length > 0
+                    ? await getSuggestions(inputValue)
+                    : popularPackages;
+            setInputItems(packages);
         },
     });
 
