d79ea3b9454d36c5b72cdd44165a4a32d09509dc
Oscar Busk <oscar.busk@gmail.com>
2021-08-10 19:30:09 +0200

    test: Make jest configuration more advanced, following examples

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,19 @@
 module.exports = {
+    roots: ["<rootDir>"],
     testEnvironment: "jsdom",
-    moduleDirectories: ["node_modules", "src"],
-    collectCoverageFrom: [
-        "**/*.{js,jsx,ts,tsx}",
-        "!**/*.d.ts",
-        "!**/node_modules/**",
+    moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+    watchPlugins: [
+        "jest-watch-typeahead/filename",
+        "jest-watch-typeahead/testname",
     ],
-    coverageDirectory: "coverage",
-    testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-    transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    moduleDirectories: ["node_modules", "src"],
+    moduleNameMapper: {
+        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+        "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
     },
+    testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    },
+    transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
 };
