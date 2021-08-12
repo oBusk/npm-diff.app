94164106dbdac22f3b10e59625a384673405505b
Oscar Busk <oscar.busk@gmail.com>
2021-08-12 23:50:21 +0200

    fix: Fix useThrottle using incorrect signature

--- src/lib/utils/useThrottle.ts
+++ src/lib/utils/useThrottle.ts
@@ -1,13 +1,9 @@
 import { useMemo } from "react";
 import throttle from "./throttle";
 
-function useThrottle<F extends (...args: any) => void>(
-    fn: F,
-    wait: number,
-    leading = false,
-): F {
+function useThrottle<F extends (...args: any) => void>(fn: F, wait: number): F {
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    return useMemo(() => throttle(fn, wait, leading), []);
+    return useMemo(() => throttle(fn, wait), []);
 }
 
 export default useThrottle;
