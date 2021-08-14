abac35ee1f5292ce728a496079d3535187d3ebdd
Oscar Busk <oscar.busk@gmail.com>
2021-08-14 16:27:56 +0200

    feat: Build basic version filtering in autocompelte

--- src/lib/autocomplete/providers/getAutocompleteVersions.ts
+++ src/lib/autocomplete/providers/getAutocompleteVersions.ts
@@ -6,17 +6,23 @@ import AutocompleteSuggestion from "../AutocompleteSuggestion";
 async function getAutocompleteVersions(
     query: string,
 ): Promise<AutocompleteSuggestion[]> {
-    const { name } = npa(query);
+    const { name, rawSpec } = npa(query);
 
     const response = await fetch(`/api/versions?spec=${name}`);
     const versions: ApiVersionsResponse = await response.json();
 
-    return versions
-        .reverse()
-        .slice(0, AUTOCOMPLETE_SIZE)
-        .map(({ name, version }) => ({
-            name: `${name}@${version}`,
-        }));
+    return (
+        versions
+            .reverse()
+            // TODO: Instead of just using `filter()` here, we should have some util
+            // to "filter until". Since we only care about the first `AUTOCOMPLETE_SIZE`
+            // number of hits, we want to stop running once we've found that many.
+            .filter(({ version }) => version.startsWith(rawSpec))
+            .slice(0, AUTOCOMPLETE_SIZE)
+            .map(({ name, version }) => ({
+                name: `${name}@${version}`,
+            }))
+    );
 }
 
 export default getAutocompleteVersions;
