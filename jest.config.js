218dab6d093f7532f7525e078dd88db4e41af157
Oscar Busk <oscar.busk@gmail.com>
2021-10-04 16:38:39 +0200

    chore: stop trying to be too clever

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-    collectCoverageFrom: ["**/*.[jt]sx?$", "!**/*.d.ts", "!**/node_modules/**"],
+    collectCoverageFrom: ["src/**/*.{js,ts,jsx,tsx}", "!**/*.d.ts"],
     moduleDirectories: ["node_modules", "src"],
     moduleNameMapper: {
         // Handle CSS imports (with CSS modules)
         // https://jestjs.io/docs/webpack#mocking-css-modules
-        "^.+\\.module\\.(c|sa|sc)ss$": "identity-obj-proxy",
+        "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
 
         // Handle CSS imports (without CSS modules)
-        "^.+\\.(c|sa|sc)ss$": "<rootDir>/__mocks__/styleMock.js",
+        "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
 
         // Handle image imports
         // https://jestjs.io/docs/webpack#handling-static-assets
         "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
     },
     testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-    transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(c|sa|sc)ss$"],
+    transformIgnorePatterns: [
+        "/node_modules/",
+        "^.+\\.module\\.(css|sass|scss)$",
+    ],
 };
