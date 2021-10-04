d921d97fe2ccf03845e28b5e8612596d359a8b74
Oscar Busk <oscar.busk@gmail.com>
2021-10-04 16:56:12 +0200

    test: Restructure testing setup.
    Mostly following https://github.com/vercel/next.js/tree/6a32d854fcbb8bb513371db0584375c17c219815/examples/with-jest
    
    Preparation for testing of react components and hooks

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,21 @@
 module.exports = {
+    collectCoverageFrom: ["src/**/*.{js,ts,jsx,tsx}", "!**/*.d.ts"],
     moduleDirectories: ["node_modules", "src"],
-    collectCoverageFrom: [
-        "**/*.{js,jsx,ts,tsx}",
-        "!**/*.d.ts",
-        "!**/node_modules/**",
-    ],
-    coverageDirectory: "coverage",
-    testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-    transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    moduleNameMapper: {
+        // Handle CSS imports (with CSS modules)
+        // https://jestjs.io/docs/webpack#mocking-css-modules
+        "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+
+        // Handle CSS imports (without CSS modules)
+        "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+
+        // Handle image imports
+        // https://jestjs.io/docs/webpack#handling-static-assets
+        "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
     },
+    testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+    transformIgnorePatterns: [
+        "/node_modules/",
+        "^.+\\.module\\.(css|sass|scss)$",
+    ],
 };
