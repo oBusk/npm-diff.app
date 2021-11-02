0cb09aeb5462c930b43fe681d834cc65eae2feef
Oscar Busk <oscar.busk@gmail.com>
2021-11-02 02:43:24 +0100

    Add cache to the middleware, and add some feedback

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -7,7 +7,11 @@ type Middleware = (
     res?: NextFetchEvent,
 ) => Promise<Response | undefined> | Response | undefined;
 
+let requestNumber = 0;
+
 const router: Middleware = (request) => {
+    requestNumber++;
+    console.log(`[${requestNumber}] ${request.url}`);
     switch (request?.nextUrl?.pathname) {
         case "/api/versions":
             return versions(request);
@@ -16,19 +20,32 @@ const router: Middleware = (request) => {
 
 export type ApiVersionsResponse = Array<Version>;
 
+const versionsCache = new Map<string, Promise<ApiVersionsResponse>>();
+
 const versions: Middleware = async (request) => {
+    const start = Date.now();
     const spec = request.nextUrl.searchParams.get("spec");
 
     if (spec == null) {
         throw new Error("spec is required");
     }
 
-    const versions = await getAllVersions(Array.isArray(spec) ? spec[0] : spec);
+    const name = Array.isArray(spec) ? spec[0] : spec;
 
-    return new Response(JSON.stringify(versions), {
+    const cached = versionsCache.has(name);
+
+    if (!cached) {
+        versionsCache.set(name, getAllVersions(name));
+    }
+
+    return new Response(JSON.stringify(await versionsCache.get(name)), {
         status: 200,
         headers: {
             "Content-Type": "application/json",
+            "x-cached-in-middleware": cached ? "true" : "false",
+            "x-cache-size": `${versionsCache.size}`,
+            "x-handled": `${requestNumber}`,
+            "x-request-time-ms": `${Date.now() - start}`,
             ...responseCacheSwr,
         },
     });
