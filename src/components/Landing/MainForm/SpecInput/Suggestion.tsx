b760b4f6b97ed345b6d6ae90d0f3eacab58fa0ab
Oscar Busk <oscar.busk@gmail.com>
2021-11-07 03:11:23 +0100

    Stop using dangorouslySettingHtml

--- src/components/Landing/MainForm/SpecInput/Suggestion.tsx
+++ src/components/Landing/MainForm/SpecInput/Suggestion.tsx
@@ -2,26 +2,32 @@ import { chakra, Code, HStack, Tag, Text } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 import { AutocompleteSuggestion } from "^/lib/autocomplete";
 
-const SuggestionTitle = chakra(Code, {
+const Em = chakra("em", {
     baseStyle: {
-        // `npms` wraps the matching part of the package name with `<em>`.
-        // The Italic looks a bit too discreet, so let's remove italic
-        // and add underline instead.
-        em: {
-            fontStyle: "normal",
-            textDecoration: "underline",
-        },
+        fontStyle: "normal",
+        fontWeight: "bold",
+        textDecoration: "underline",
     },
 });
 
-const SuggestionTag = chakra(Tag, {
-    baseStyle: {
-        em: {
-            fontStyle: "normal",
-            fontWeight: "bold",
-        },
-    },
-});
+const Emphasized: FunctionComponent<{ text?: string }> = ({
+    text = "NO_TEXT",
+}) => {
+    const [before, rest] = text.split("<em>");
+    if (rest == null) {
+        // If there is no emphasis, just return the text
+        return <>{text}</>;
+    }
+    const [em, after] = rest.split("</em>");
+
+    return (
+        <>
+            {before}
+            <Em>{em}</Em>
+            {after}
+        </>
+    );
+};
 
 export interface SuggestionProps {
     item?: AutocompleteSuggestion;
@@ -32,27 +38,17 @@ const Suggestion: FunctionComponent<SuggestionProps> = ({
     item: { title, body, titleWithHighlight, tags } = {},
 }) => (
     <>
-        {titleWithHighlight ? (
-            <SuggestionTitle
-                dangerouslySetInnerHTML={{
-                    __html: titleWithHighlight,
-                }}
-            ></SuggestionTitle>
-        ) : (
-            <SuggestionTitle>{title}</SuggestionTitle>
-        )}
+        <Code>
+            <Emphasized text={titleWithHighlight ?? title} />
+        </Code>
 
         {body && <Text>{body}</Text>}
         {tags && (
             <HStack marginTop="5px">
                 {tags.map((tag) => (
-                    <SuggestionTag
-                        key={tag}
-                        variant="outline"
-                        dangerouslySetInnerHTML={{
-                            __html: tag,
-                        }}
-                    />
+                    <Tag key={tag} variant="outline">
+                        <Emphasized text={tag} />
+                    </Tag>
                 ))}
             </HStack>
         )}
