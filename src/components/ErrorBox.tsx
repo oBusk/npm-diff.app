500ff73d44456f4d9140dcc0810d4063e9c8553d
Oscar Busk <oscar.busk@gmail.com>
2022-01-17 01:23:22 +0100

    Show error when using bad query

---
+++ src/components/ErrorBox.tsx
@@ -0,0 +1,12 @@
+import { Code, forwardRef, useColorModeValue } from "@chakra-ui/react";
+import BorderBox, { BorderBoxProps } from "./theme/BorderBox";
+
+export interface ErrorBoxProps extends BorderBoxProps {}
+
+const ErrorBox = forwardRef<ErrorBoxProps, typeof BorderBox>((props, ref) => {
+    const errorBackground = useColorModeValue("red.200", "red.700");
+
+    return <BorderBox as={Code} backgroundColor={errorBackground} {...props} />;
+});
+
+export default ErrorBox;
---
+++ src/lib/diff.ts
@@ -0,0 +1,54 @@
+import libnpmdiff, { Error404, ErrorETARGET, Options } from "libnpmdiff";
+
+export interface DiffError {
+    code: number;
+    error: string;
+}
+
+async function doDiff(
+    specs: [string, string],
+    options: Options,
+): Promise<string> {
+    try {
+        const result = await libnpmdiff(specs, options);
+
+        return result;
+    } catch (e: any) {
+        console.error("DIFF ERROR", e);
+
+        const isEtarget = (e: any): e is ErrorETARGET => e.code === "ETARGET";
+        const isE404 = (e: any): e is Error404 => e.code === "E404";
+
+        if (isEtarget(e)) {
+            const code = 404;
+
+            if (e.type === "version") {
+                throw {
+                    code,
+                    error: `Could not find version ${e.wanted}.`,
+                };
+            }
+
+            throw {
+                code,
+                error: `Could not find`,
+            };
+        }
+
+        if (isE404(e)) {
+            const code = 404;
+
+            throw {
+                code,
+                error: `Could not find package that matches "${e.pkgid}"`,
+            };
+        }
+
+        throw {
+            code: 500,
+            error: "Unknown error",
+        };
+    }
+}
+
+export default doDiff;
