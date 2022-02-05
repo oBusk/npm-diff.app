1d78a952db1d1e4362a04023f5f0c6c4af225f02
Oscar Busk <oscar.busk@gmail.com>
2022-02-05 17:27:34 +0100

    Use next/jest for testing (#537)
    * Use next/jest
    
    * Make sure that testing react componentss works

---
+++ src/components/theme/ExternalLink.test.tsx
@@ -0,0 +1,18 @@
+/** @jest-environment jsdom */
+
+import { render, screen } from "@testing-library/react";
+import ExternalLink from "./ExternalLink";
+
+describe("ExternalLink", () => {
+    it("has noreferrer noopener", () => {
+        render(
+            <ExternalLink href="https://example.com">
+                Example Link
+            </ExternalLink>,
+        );
+
+        const link = screen.getByText("Example Link");
+
+        expect(link).toHaveAttribute("rel", "noreferrer noopener");
+    });
+});
