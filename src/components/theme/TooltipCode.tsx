95964074fe759760b7b20324418e27c5b8f7df99
Oscar Busk <oscar.busk@gmail.com>
2022-02-06 02:10:57 +0100

    Document TooltipCode

--- src/components/theme/TooltipCode.tsx
+++ src/components/theme/TooltipCode.tsx
@@ -5,6 +5,7 @@ import {
     useColorModeValue,
 } from "@chakra-ui/react";
 
+/** Small wrapper for https://chakra-ui.com/code for better colors in Tooltip */
 const TooltipCode = forwardRef<CodeProps, typeof Code>((props, ref) => {
     // Since the tooltip is dark in light mode and dark in light mode, we invert the colors
     // Looking at colors in https://github.com/chakra-ui/chakra-ui/blob/%40chakra-ui/react%401.8.3/packages/theme/src/components/badge.ts
