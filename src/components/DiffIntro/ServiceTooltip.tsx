0610c6f1c755ab47463f16001a8b7d176c3eff32
Oscar Busk <oscar.busk@gmail.com>
2022-02-06 01:43:58 +0100

    Fix code element in serviceTooltip.
    Fixes oBusk/npm-diff.app#482

--- src/components/DiffIntro/ServiceTooltip.tsx
+++ src/components/DiffIntro/ServiceTooltip.tsx
@@ -4,6 +4,7 @@ import {
     Text,
     Tooltip,
     TooltipProps,
+    useColorModeValue,
 } from "@chakra-ui/react";
 import B from "^/components/theme/B";
 
@@ -14,26 +15,33 @@ export interface ServiceTooltipProps extends TooltipProps {
 }
 
 const ServiceTooltip = forwardRef<ServiceTooltipProps, any>(
-    ({ packageName, packageVersion, serviceName, ...props }, ref) => (
-        <Tooltip
-            textAlign="center"
-            {...props}
-            ref={ref}
-            label={
-                <>
-                    <Text whiteSpace="nowrap">
-                        View{" "}
-                        <Code>
-                            {packageName}@{packageVersion}
-                        </Code>
-                    </Text>
-                    <Text>
-                        on <B>{serviceName}</B>
-                    </Text>
-                </>
-            }
-        />
-    ),
+    ({ packageName, packageVersion, serviceName, ...props }, ref) => {
+        // Since the tooltip is dark in light mode and dark in light mode, we invert the colors
+        // Looking at colors in https://github.com/chakra-ui/chakra-ui/blob/%40chakra-ui/react%401.8.3/packages/theme/src/components/badge.ts
+        const codeBg = useColorModeValue("gray.200", "gray.700");
+        const codeColor = useColorModeValue("gray.800", "gray.200");
+
+        return (
+            <Tooltip
+                textAlign="center"
+                {...props}
+                ref={ref}
+                label={
+                    <>
+                        <Text whiteSpace="nowrap">
+                            View{" "}
+                            <Code bg={codeBg} color={codeColor}>
+                                {packageName}@{packageVersion}
+                            </Code>
+                        </Text>
+                        <Text>
+                            on <B>{serviceName}</B>
+                        </Text>
+                    </>
+                }
+            />
+        );
+    },
 );
 
 export default ServiceTooltip;
