0865d7b73a1059a576759cec8195aa588c2f5507
Oscar Busk <oscar.busk@gmail.com>
2022-04-19 11:23:47 +0200

    chore(test): Replace usage of tl/react-hooks to tl/react.
    https://github.com/testing-library/react-hooks-testing-library/issues/654#issuecomment-1100377254

--- src/lib/hooks/useThrottle.test.ts
+++ src/lib/hooks/useThrottle.test.ts
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react";
 import * as throttle from "../utils/throttle";
 import useThrottle from "./useThrottle";
 
