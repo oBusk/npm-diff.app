835c02596e259511af480bd756ebca251588ac8c
Oscar Busk <oscar.busk@gmail.com>
2022-06-20 03:28:51 +0200

    Update MainForm to use forwardRef

--- src/components/Landing/MainForm/MainForm.tsx
+++ src/components/Landing/MainForm/MainForm.tsx
@@ -1,5 +1,6 @@
 import {
     Button,
+    Flex,
     forwardRef,
     Input,
     InputProps,
@@ -30,119 +31,119 @@ export interface MainFormProps extends StackProps {
     handleSubmit: (a: string | undefined, b: string | undefined) => void;
 }
 
-const MainForm: FunctionComponent<MainFormProps> = ({
-    overrideA,
-    overrideB,
-    children,
-    isLoading,
-    handleSubmit,
-    ...props
-}) => {
-    const aRef = useRef<HTMLInputElement>(null);
-    const [a, setA] = useState("");
-    const [b, setB] = useState("");
+const MainForm = forwardRef<MainFormProps, typeof Flex>(
+    (
+        { overrideA, overrideB, children, isLoading, handleSubmit, ...props },
+        ref,
+    ) => {
+        const aRef = useRef<HTMLInputElement>(null);
+        const [a, setA] = useState("");
+        const [b, setB] = useState("");
 
-    /**
-     * A placeholder value for `B` if nothing is entered in `B`.
-     *
-     * This will be the package of `A` with the `latest` tag.
-     */
-    const automaticB = useMemo(() => {
-        try {
-            if (b) {
+        /**
+         * A placeholder value for `B` if nothing is entered in `B`.
+         *
+         * This will be the package of `A` with the `latest` tag.
+         */
+        const automaticB = useMemo(() => {
+            try {
+                if (b) {
+                    return null;
+                }
+                const aName = npa(a)?.name;
+                if (!aName) {
+                    return null;
+                }
+                return `${aName}@latest`;
+            } catch (e) {
                 return null;
             }
-            const aName = npa(a)?.name;
-            if (!aName) {
-                return null;
-            }
-            return `${aName}@latest`;
-        } catch (e) {
-            return null;
-        }
-    }, [a, b]);
+        }, [a, b]);
 
-    useEffect(() => {
-        // Focus the input on initial load (only)
-        aRef.current?.focus();
-    }, []);
+        useEffect(() => {
+            // Focus the input on initial load (only)
+            aRef.current?.focus();
+        }, []);
 
-    const internalHandleSubmit = (event: FormEvent): void => {
-        event.preventDefault();
+        const internalHandleSubmit = (event: FormEvent): void => {
+            event.preventDefault();
 
-        const target = event.target as typeof event.target & {
-            a: HTMLInputElement;
-            b: HTMLInputElement;
+            const target = event.target as typeof event.target & {
+                a: HTMLInputElement;
+                b: HTMLInputElement;
+            };
+
+            handleSubmit(target.a.value, target.b.value);
         };
 
-        handleSubmit(target.a.value, target.b.value);
-    };
-
-    return (
-        <Stack
-            as="form"
-            onSubmit={internalHandleSubmit}
-            align="center"
-            justify="center"
-            direction={{ base: "column", lg: "row" }}
-            spacing={{ base: "0.5rem", lg: 0 }}
-            {...props}
-        >
-            <Tooltip
-                label={`The specification of the base, like "package@1.2.3"`}
-                closeOnClick={false}
+        return (
+            <Stack
+                as="form"
+                onSubmit={internalHandleSubmit}
+                align="center"
+                justify="center"
+                direction={{ base: "column", lg: "row" }}
+                spacing={{ base: "0.5rem", lg: 0 }}
+                ref={ref}
+                {...props}
             >
-                <SpecInput
-                    name="a"
-                    placeholder="package@1.2.3 or package@^1"
-                    disabled={overrideA != null || isLoading}
-                    value={overrideA ?? a ?? ""}
-                    onChange={(event) => setA(event.target.value)}
-                    ref={aRef}
-                    borderEndRadius={{ lg: 0 }}
-                ></SpecInput>
-            </Tooltip>
-            <CenterInputAddon display={{ base: "none", lg: "flex" }}>
-                ...
-            </CenterInputAddon>
-            <Tooltip
-                label={`The specification of the compare, like "package"`}
-                closeOnClick={false}
-            >
-                <SpecInput
-                    name="b"
-                    placeholder={automaticB || undefined}
-                    disabled={overrideB != null || isLoading}
-                    value={overrideB ?? b ?? ""}
-                    onChange={(event) => setB(event.target.value)}
-                    borderStartRadius={{ lg: 0 }}
-                ></SpecInput>
-            </Tooltip>
-            <Tooltip
-                label={
-                    !a ? (
-                        "Enter a package specification to compare"
-                    ) : (
-                        <>
-                            Compare <TooltipCode>{a}</TooltipCode> and{" "}
-                            <TooltipCode>{automaticB || b}</TooltipCode> now!
-                        </>
-                    )
-                }
-                background={!a ? "red.700" : undefined}
-                shouldWrapChildren
-            >
-                <Button
-                    isLoading={isLoading}
-                    type="submit"
-                    disabled={!a}
-                    marginInlineStart={{ lg: "2rem" }}
+                <Tooltip
+                    label={`The specification of the base, like "package@1.2.3"`}
+                    closeOnClick={false}
                 >
-                    npm diff! 📦🔃
-                </Button>
-            </Tooltip>
-        </Stack>
-    );
-};
+                    <SpecInput
+                        name="a"
+                        placeholder="package@1.2.3 or package@^1"
+                        disabled={overrideA != null || isLoading}
+                        value={overrideA ?? a ?? ""}
+                        onChange={(event) => setA(event.target.value)}
+                        ref={aRef}
+                        borderEndRadius={{ lg: 0 }}
+                    ></SpecInput>
+                </Tooltip>
+                <CenterInputAddon display={{ base: "none", lg: "flex" }}>
+                    ...
+                </CenterInputAddon>
+                <Tooltip
+                    label={`The specification of the compare, like "package"`}
+                    closeOnClick={false}
+                >
+                    <SpecInput
+                        name="b"
+                        placeholder={automaticB || undefined}
+                        disabled={overrideB != null || isLoading}
+                        value={overrideB ?? b ?? ""}
+                        onChange={(event) => setB(event.target.value)}
+                        borderStartRadius={{ lg: 0 }}
+                    ></SpecInput>
+                </Tooltip>
+                <Tooltip
+                    label={
+                        !a ? (
+                            "Enter a package specification to compare"
+                        ) : (
+                            <>
+                                Compare <TooltipCode>{a}</TooltipCode> and{" "}
+                                <TooltipCode>{automaticB || b}</TooltipCode>{" "}
+                                now!
+                            </>
+                        )
+                    }
+                    background={!a ? "red.700" : undefined}
+                    shouldWrapChildren
+                >
+                    <Button
+                        isLoading={isLoading}
+                        type="submit"
+                        disabled={!a}
+                        marginInlineStart={{ lg: "2rem" }}
+                    >
+                        npm diff! 📦🔃
+                    </Button>
+                </Tooltip>
+            </Stack>
+        );
+    },
+);
 
 export default MainForm;
