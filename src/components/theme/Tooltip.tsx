2004fa5f477f23f6d13e329f5eb44299da0b9c4b
Oscar Busk <oscar.busk@gmail.com>
2022-06-21 15:27:46 +0200

    Add arrow on tooltip

--- src/components/theme/Tooltip.tsx
+++ src/components/theme/Tooltip.tsx
@@ -1,16 +1,19 @@
 import {
-    chakra,
     Tooltip as ChakraTooltip,
     TooltipProps as ChakraTooltipProps,
+    forwardRef,
 } from "@chakra-ui/react";
 
 export interface TooltipProps extends ChakraTooltipProps {}
 
-const Tooltip = chakra(ChakraTooltip, {
-    baseStyle: {
-        maxWidth: "600px",
-        padding: "4px",
-    },
-});
+const Tooltip = forwardRef<TooltipProps, "div">((props, ref) => (
+    <ChakraTooltip
+        maxWidth="600px"
+        padding="4px"
+        hasArrow
+        {...props}
+        ref={ref}
+    />
+));
 
 export default Tooltip;
