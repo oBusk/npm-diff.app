2b8503129a05dfbba8f49016777a2881283956c7
Oscar Busk <oscar.busk@gmail.com>
2022-06-23 01:54:20 +0200

    Replace local type for validate-npm-package-name

--- types/validate-npm-package-name/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare function validatePackageName(name: string): {
-    validForNewPackages: boolean;
-    validForOldPackages: boolean;
-    warnings: string[];
-    errors: string[];
-};
-
-export default validatePackageName;
