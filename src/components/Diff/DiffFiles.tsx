af8d47ff443e22d2cecfb6a53d6202887076811c
Oscar Busk <oscar.busk@gmail.com>
2022-06-25 22:49:08 +0200

    First implementation of virutal scrolling using react-cool-virtual

--- src/components/Diff/DiffFiles.tsx
+++ src/components/Diff/DiffFiles.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
+import useVirtual from "react-cool-virtual";
 import { File } from "react-diff-view";
 import DiffFileComponent from "./DiffFile";
 
@@ -17,18 +18,32 @@ function hashFromString(s: string): string {
         .toString(36);
 }
 
-const DiffFiles: FunctionComponent<Props> = ({ files }) => (
-    <Box minWidth="100%">
-        {files.map(({ newPath, newRevision, type, hunks }) => (
-            <DiffFileComponent
-                key={`${newPath}|${newRevision}`}
-                title={newPath}
-                type={type}
-                hunks={hunks}
-                hash={hashFromString(newPath)}
-            />
-        ))}
-    </Box>
-);
+const DiffFiles: FunctionComponent<Props> = ({ files }) => {
+    const { outerRef, innerRef, items } = useVirtual({
+        itemCount: files.length,
+        itemSize: 187, // the size of the "load diff" placeholder
+        ssrItemCount: 10,
+    });
+
+    return (
+        <Box minWidth="100%" ref={outerRef as any}>
+            <Box ref={innerRef as any}>
+                {items.map(({ index, measureRef }) => {
+                    const { newPath, newRevision, type, hunks } = files[index];
+                    return (
+                        <DiffFileComponent
+                            key={`${newPath}|${newRevision}`}
+                            ref={measureRef}
+                            title={newPath}
+                            type={type}
+                            hunks={hunks}
+                            hash={hashFromString(newPath)}
+                        />
+                    );
+                })}
+            </Box>
+        </Box>
+    );
+};
 
 export default DiffFiles;
