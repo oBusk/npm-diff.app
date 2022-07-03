e70797efff2e871f973244bcd8de2c4b9fab66b6
Oscar Busk <oscar.busk@gmail.com>
2022-07-03 15:37:12 +0200

    Use a different icon for npm link

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -1,6 +1,6 @@
 // Re-export some icons to ensure using the same icon in all uses
 
-export { DiNpm as NpmIcon } from "react-icons/di";
+export { RiNpmjsLine as NpmIcon } from "react-icons/ri";
 export {
     FaBox as PackagephobiaIcon,
     FaCloud as UNPKGIcon,
