f4269b47beb1f42a924097b9f15c42929f82be88
Oscar Busk <oscar.busk@gmail.com>
2022-08-01 00:14:51 +0200

    Improve first render by defaulting for desktop and memoizing

--- src/pages/[...parts].tsx
+++ src/pages/[...parts].tsx
@@ -1,9 +1,10 @@
 import { Center, useBreakpointValue } from "@chakra-ui/react";
 import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
-import npa from "npm-package-arg";
+import npa, { Result as NpaResult } from "npm-package-arg";
 import { ParsedUrlQuery } from "querystring";
-import { useMemo } from "react";
+import { memo, useMemo, useState } from "react";
+import type { File } from "react-diff-view";
 import { parseDiff, ViewType } from "react-diff-view";
 import DiffFiles from "^/components/Diff/DiffFiles";
 import DiffIntro from "^/components/DiffIntro";
@@ -146,19 +147,53 @@ export const getServerSideProps: GetServerSideProps<Props, Params> = async ({
 };
 
 const DiffPage: NextPage<Props> = ({ error, result }) => {
+    const { diff, packagephobiaResults, bundlephobiaResults, options } =
+        result!;
+
     const router = useRouter();
-    const defaultViewType: ViewType = useBreakpointValue({
-        base: "unified",
-        lg: "split",
-    })!;
+    // Even if the initial value and the first breakpoint value is the same,
+    // the component will re-render. This means it will _always_ render twice
+    // even when it shouldn't have to.
+    // We work around this by memoizing the rendering of the component.
+    const defaultViewType = useBreakpointValue<ViewType>(
+        {
+            base: "unified",
+            lg: "split",
+        },
+        // We assume that most users are on a computer so default to "lg".
+        // We could use something like https://github.com/kaimallea/isMobile
+        // but that means cache should be different for desktop/mobile
+        "lg",
+    )!;
+
     const [a, b] = result?.specs ?? [];
     const aNpa = useMemo(() => (a ? npa(a) : undefined), [a]);
     const bNpa = useMemo(() => (b ? npa(b) : undefined), [b]);
+    const files = useMemo(() => {
+        if (diff) {
+            const adjustedDiff = adjustDiff(diff);
+            if (adjustedDiff) {
+                return parseDiff(adjustedDiff);
+            }
+        }
+
+        return null;
+    }, [diff]);
 
     if (aNpa === undefined || bNpa === undefined) {
         return (
             <Layout title="Error">
                 <ErrorBox>Specs could not be parsed</ErrorBox>
+            </Layout>
+        );
+    }
+
+    if (files == null) {
+        return (
+            <Layout title="Error">
+                <Center>
+                    <ErrorBox>Files could not be parsed</ErrorBox>
+                </Center>
             </Layout>
         );
     }
@@ -172,12 +207,6 @@ const DiffPage: NextPage<Props> = ({ error, result }) => {
             </Layout>
         );
     }
-
-    const { diff, packagephobiaResults, bundlephobiaResults, options } =
-        result!;
-
-    const adjustedDiff = adjustDiff(diff);
-    const files = parseDiff(adjustedDiff);
 
     const viewType =
         // If specified in URL, use that
@@ -193,19 +222,51 @@ const DiffPage: NextPage<Props> = ({ error, result }) => {
             title={`Comparing ${a}...${b}`}
             description={`A diff between the npm packages "${a}" and "${b}"`}
         >
-            <DiffIntro
-                a={aNpa}
-                b={bNpa}
+            <MemoizedDiffPageContent
+                aNpa={aNpa}
+                bNpa={bNpa}
                 files={files}
                 packagephobiaResults={packagephobiaResults}
                 bundlephobiaResults={bundlephobiaResults}
                 options={options}
                 viewType={viewType}
-                alignSelf="stretch"
             />
-            <DiffFiles a={aNpa} b={bNpa} files={files} viewType={viewType} />
         </Layout>
     );
 };
+
+const DiffPageContent = ({
+    aNpa,
+    bNpa,
+    files,
+    packagephobiaResults,
+    bundlephobiaResults,
+    options,
+    viewType,
+}: {
+    aNpa: NpaResult;
+    bNpa: NpaResult;
+    files: File[];
+    packagephobiaResults: PackagephobiaResults | null;
+    bundlephobiaResults: BundlephobiaResults | null;
+    options: DiffOptions;
+    viewType: ViewType;
+}) => (
+    <>
+        <DiffIntro
+            a={aNpa}
+            b={bNpa}
+            files={files}
+            packagephobiaResults={packagephobiaResults}
+            bundlephobiaResults={bundlephobiaResults}
+            options={options}
+            viewType={viewType}
+            alignSelf="stretch"
+        />
+        <DiffFiles a={aNpa} b={bNpa} files={files} viewType={viewType as any} />
+    </>
+);
+
+const MemoizedDiffPageContent = memo(DiffPageContent);
 
 export default DiffPage;
