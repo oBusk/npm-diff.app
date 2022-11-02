9959fc6343af6076e2c69224d04dfd8fc37b8a9e
Oscar Busk <oscar.busk@gmail.com>
2022-11-02 11:44:59 +0100

    Add dummy app dir page

---
+++ src/app/dummy/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+export interface DummyPageProps {}
+
+const DummyPage: NextPage<DummyPageProps> = () => <h1>App Directory!</h1>;
+
+export default DummyPage;
---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-head-element */
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html>
+            <head></head>
+            <body>{children}</body>
+        </html>
+    );
+}
