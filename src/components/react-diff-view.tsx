68a6c86146ead27dae666b569a4230c12bb07687
Oscar Busk <oscar.busk@gmail.com>
2022-11-09 20:42:55 +0100

    Move adjustments of Diff to chakra() factory call.
    Instead of global in theme.

--- src/components/react-diff-view.tsx
+++ src/components/react-diff-view.tsx
@@ -5,9 +5,33 @@ import {
     Hunk as _Hunk,
 } from "react-diff-view";
 
-// Simply wrap any component as `chakra()` so we can style them just like all other components
+// https://github.com/otakustay/react-diff-view/blob/v2.4.8/site/components/DiffView/diff.global.less#L22-L36
+// Cross referenced with
+// https://github.com/otakustay/react-diff-view/blob/v2.4.8/README.md#css-variables
+const reactDiffViewDarkModeVariables = {
+    // "--diff-background-color": "initial",
+    "--diff-text-color": "#fafafa",
+    // "--diff-font-family": "Consolas, Courier, monospace",
+    "--diff-selection-background-color": "#5a5f80",
+    "--diff-gutter-insert-background-color": "#082525",
+    "--diff-gutter-delete-background-color": "#2b1523",
+    "--diff-gutter-selected-background-color": "#5a5f80",
+    "--diff-code-insert-background-color": "#082525",
+    "--diff-code-delete-background-color": "#2b1523",
+    "--diff-code-insert-edit-background-color": "#00462f",
+    "--diff-code-delete-edit-background-color": "#4e2436",
+    "--diff-code-selected-background-color": "#5a5f80",
+    // "--diff-omit-gutter-line-color": "#cb2a1d",
+} as const;
 
-export const Diff = chakra(_Diff);
+export const Diff = chakra(_Diff, {
+    baseStyle: {
+        ".diff-gutter": {
+            scrollMarginTop: "100px",
+        },
+        _dark: { ...reactDiffViewDarkModeVariables },
+    },
+});
 
 export const Decoration = chakra(_Decoration);
 
--- src/theme.ts
+++ src/theme.ts
@@ -6,25 +6,6 @@ import {
 } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
-// https://github.com/otakustay/react-diff-view/blob/v2.4.8/site/components/DiffView/diff.global.less#L22-L36
-// Cross referenced with
-// https://github.com/otakustay/react-diff-view/blob/v2.4.8/README.md#css-variables
-const reactDiffViewDarkModeVariables = {
-    // "--diff-background-color": "initial",
-    "--diff-text-color": "#fafafa",
-    // "--diff-font-family": "Consolas, Courier, monospace",
-    "--diff-selection-background-color": "#5a5f80",
-    "--diff-gutter-insert-background-color": "#082525",
-    "--diff-gutter-delete-background-color": "#2b1523",
-    "--diff-gutter-selected-background-color": "#5a5f80",
-    "--diff-code-insert-background-color": "#082525",
-    "--diff-code-delete-background-color": "#2b1523",
-    "--diff-code-insert-edit-background-color": "#00462f",
-    "--diff-code-delete-edit-background-color": "#4e2436",
-    "--diff-code-selected-background-color": "#5a5f80",
-    // "--diff-omit-gutter-line-color": "#cb2a1d",
-} as const;
-
 const config: ThemeConfig = {
     initialColorMode: "dark",
     useSystemColorMode: true,
@@ -73,12 +54,6 @@ const themeOverride: ThemeOverride = {
         global: {
             html: {
                 minWidth: "sm",
-            },
-            ".diff-gutter": {
-                scrollMarginTop: "100px",
-            },
-            _dark: {
-                ":root": { ...reactDiffViewDarkModeVariables },
             },
         },
     },
