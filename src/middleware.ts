49d456660239a00841f944e25c26a7475648bf28
Oscar Busk <oscar.busk@gmail.com>
2022-11-16 02:01:20 +0100

    Remove middleware that should no longer be needed

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-    // Temporary workaround to make navigation between app/pages work
-    // https://github.com/vercel/next.js/issues/42364#issuecomment-1300836034
-    return NextResponse.rewrite(new URL(request.nextUrl.pathname, request.url));
-}
