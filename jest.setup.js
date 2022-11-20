bd767867cfee56c444905d165845772b2748f791
Oscar Busk <oscar.busk@gmail.com>
2022-11-20 14:18:44 +0100

    Fix tests using React.cache()

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom/extend-expect";
+
+// Override the react.cache method to avoid caching in tests
+jest.mock("react", () => {
+    const React = jest.requireActual("react");
+    React.cache = (fn) => fn;
+    return React;
+});
