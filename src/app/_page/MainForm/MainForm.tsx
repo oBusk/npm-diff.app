cd3e2e46351b4b07e5528d92618f44a2f7959bcf
Oscar Busk <oscar.busk@gmail.com>
2023-04-09 23:39:13 +0200

    chore: Remove wrong param
    (cherry picked from commit 23d873ab191938eb59c6eaf86b2d4c517f8ecf64)

--- src/app/_page/MainForm/MainForm.tsx
+++ src/app/_page/MainForm/MainForm.tsx
@@ -110,10 +110,7 @@ const MainForm = forwardRef<MainFormProps, typeof Flex>(
                     }}
                     fallbackSuggestions={fallbackSuggestions}
                 ></SpecInput>
-                <CenterInputAddon
-                    size={SIZE}
-                    display={{ base: "none", lg: "flex" }}
-                >
+                <CenterInputAddon display={{ base: "none", lg: "flex" }}>
                     ...
                 </CenterInputAddon>
                 <SpecInput
