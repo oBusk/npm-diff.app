01779a344ba87c8a80a6dcba5fff059f837c5e44
Oscar Busk <oscar.busk@gmail.com>
2023-05-01 22:49:34 +0200

    Convert ExamplesList to tailwind

--- src/app/_page/ExamplesList.tsx
+++ src/app/_page/ExamplesList.tsx
@@ -1,50 +1,60 @@
-import { Link } from "@chakra-ui/next-js";
-import { Heading, Stack, StackProps } from "@chakra-ui/react";
-import { FunctionComponent } from "react";
+import Link from "next/link";
+import { forwardRef, HTMLAttributes } from "react";
+import cn from "^/lib/cn";
 import EXAMPLES from "^/lib/examples";
 import { QueryParams } from "^/lib/query";
 
-export interface ExamplesListProps extends StackProps {
+export interface ExamplesListProps extends HTMLAttributes<HTMLDivElement> {
     exampleMouseOver: (a: string, b: string) => void;
     exampleMouseOut: () => void;
     exampleClicked: () => void;
     queryParams: QueryParams;
 }
 
-const ExamplesList: FunctionComponent<ExamplesListProps> = ({
-    exampleMouseOver,
-    exampleMouseOut: onMouseOut,
-    exampleClicked: onClick,
-    queryParams: query,
-    ...props
-}) => {
-    const onMouseOver = (example: string) => {
-        const [a, b] = example.split("...");
+const ExamplesList = forwardRef<HTMLDivElement, ExamplesListProps>(
+    (
+        {
+            exampleMouseOver,
+            exampleMouseOut: onMouseOut,
+            exampleClicked: onClick,
+            queryParams: query,
+            className,
+            ...props
+        },
+        ref,
+    ) => {
+        const onMouseOver = (example: string) => {
+            const [a, b] = example.split("...");
 
-        exampleMouseOver(a, b);
-    };
+            exampleMouseOver(a, b);
+        };
 
-    return (
-        <Stack align="center" fontSize="sm" {...props}>
-            <Heading color="gray.300" size="md">
-                Examples
-            </Heading>
-            {EXAMPLES.map((ex) => (
-                <Link
-                    key={ex}
-                    href={{
-                        pathname: `/${ex}`,
-                        query: { ...query },
-                    }}
-                    onMouseOver={() => onMouseOver(ex)}
-                    onMouseOut={() => onMouseOut()}
-                    onClick={() => onClick()}
-                >
-                    {ex}
-                </Link>
-            ))}
-        </Stack>
-    );
-};
+        return (
+            <section
+                className={cn("flex flex-col items-center", className)}
+                {...props}
+                ref={ref}
+            >
+                <h2 className={cn("text-xl font-bold")}>Examples</h2>
+                {EXAMPLES.map((ex) => (
+                    <Link
+                        className={cn("my-1 hover:underline")}
+                        key={ex}
+                        href={{
+                            pathname: `/${ex}`,
+                            query: { ...query },
+                        }}
+                        onMouseOver={() => onMouseOver(ex)}
+                        onMouseOut={() => onMouseOut()}
+                        onClick={() => onClick()}
+                    >
+                        {ex}
+                    </Link>
+                ))}
+            </section>
+        );
+    },
+);
+ExamplesList.displayName = "ExamplesList";
 
 export default ExamplesList;
