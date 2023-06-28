9cf3875497754f6f98c82dd819537e92e3b4b4a2
Oscar Busk <oscar.busk@gmail.com>
2023-06-29 01:22:03 +0200

    Modify page to no longer do any redirection

--- src/app/diff/[a]/[b]/page.tsx
+++ src/app/diff/[a]/[b]/page.tsx
@@ -1,13 +1,8 @@
-import { redirect } from "next/navigation";
 import { type ViewType } from "react-diff-view";
 import { createSimplePackageSpec } from "^/lib/createSimplePackageSpec";
 import { DEFAULT_DIFF_FILES_GLOB } from "^/lib/default-diff-files";
-import destination from "^/lib/destination";
 import { parseQuery, type QueryParams } from "^/lib/query";
 import { simplePackageSpecToString } from "^/lib/SimplePackageSpec";
-import decodeParts from "^/lib/utils/decodeParts";
-import specsToDiff from "^/lib/utils/specsToDiff";
-import splitParts from "^/lib/utils/splitParts";
 import BundlephobiaDiff from "./_page/BundlephobiaDiff";
 import DiffIntro from "./_page/DiffIntro";
 import NpmDiff from "./_page/NpmDiff";
@@ -15,16 +10,18 @@ import PackagephobiaDiff from "./_page/PackagephobiaDiff";
 import { type DIFF_TYPE_PARAM_NAME } from "./_page/paramNames";
 
 export interface DiffPageProps {
-    params: { a: string; b: string };
-    searchParams: QueryParams & { [DIFF_TYPE_PARAM_NAME]: ViewType };
+    params: {
+        a: string;
+        b: string;
+    };
+    searchParams: QueryParams & {
+        [DIFF_TYPE_PARAM_NAME]: ViewType;
+    };
 }
 
 export function generateMetadata({ params: { a: _a, b: _b } }: DiffPageProps) {
-    _a = decodeURIComponent(_a);
-    _b = decodeURIComponent(_b);
-
-    const a = createSimplePackageSpec(_a);
-    const b = createSimplePackageSpec(_b);
+    const a = createSimplePackageSpec(decodeURIComponent(_a));
+    const b = createSimplePackageSpec(decodeURIComponent(_b));
 
     return {
         title: `Comparing ${simplePackageSpecToString(a)}...${simplePackageSpecToString(b)}`,
@@ -38,73 +35,53 @@ const DiffPage = async ({
 }: DiffPageProps): Promise<JSX.Element> => {
     const { diffFiles, ...optionsQuery } = searchParams;
 
-    const specsOrVersions: [string, string] = [
-        decodeURIComponent(_a),
-        decodeURIComponent(_b),
-    ];
-    const { redirect: redirectTarget, canonicalSpecs } =
-        await destination(specsOrVersions);
+    const options = parseQuery({
+        // If no diffFiles is passed, use the default.
+        // This is done here, since we don't want a fall back in the API
+        diffFiles: diffFiles ?? DEFAULT_DIFF_FILES_GLOB,
+        ...optionsQuery,
+    });
 
-    if (redirectTarget !== false) {
-        const specsStr = specsToDiff(canonicalSpecs);
-        const searchStr = Object.entries(searchParams)
-            .map(([key, value]) => `${key}=${value}`)
-            .join("&");
+    _a = decodeURIComponent(_a);
+    _b = decodeURIComponent(_b);
 
-        redirect(
-            `/${specsStr}` + (searchStr?.length > 0 ? `?${searchStr}` : ""),
-        );
-    } else {
-        const options = parseQuery({
-            // If no diffFiles is passed, use the default.
-            // This is done here, since we don't want a fall back in the API
-            diffFiles: diffFiles ?? DEFAULT_DIFF_FILES_GLOB,
-            ...optionsQuery,
-        });
+    const specs = [_a, _b] as [string, string];
+    const a = createSimplePackageSpec(_a);
+    const b = createSimplePackageSpec(_b);
 
-        const [a, b] = canonicalSpecs.map((spec) =>
-            createSimplePackageSpec(spec),
-        );
-
-        return (
-            <>
-                <DiffIntro
-                    className="self-stretch"
-                    a={a}
-                    b={b}
-                    services={
-                        <>
-                            <BundlephobiaDiff
-                                a={a}
-                                b={b}
-                                specs={canonicalSpecs}
-                                key={
-                                    "bundlephobia-" + canonicalSpecs.join("...")
-                                }
-                            />
-                            <PackagephobiaDiff
-                                a={a}
-                                b={b}
-                                specs={canonicalSpecs}
-                                key={
-                                    "packagephobia-" +
-                                    canonicalSpecs.join("...")
-                                }
-                            />
-                        </>
-                    }
-                    options={options}
-                />
-                <NpmDiff
-                    a={a}
-                    b={b}
-                    specs={canonicalSpecs}
-                    options={options}
-                    key={JSON.stringify([canonicalSpecs, options])}
-                />
-            </>
-        );
-    }
+    return (
+        <>
+            <DiffIntro
+                className="self-stretch"
+                a={a}
+                b={b}
+                services={
+                    <>
+                        <BundlephobiaDiff
+                            a={a}
+                            b={b}
+                            specs={specs}
+                            key={"bundlephobia-" + specs.join("...")}
+                        />
+                        <PackagephobiaDiff
+                            a={a}
+                            b={b}
+                            specs={specs}
+                            key={"packagephobia-" + specs.join("...")}
+                        />
+                    </>
+                }
+                options={options}
+            />
+            <NpmDiff
+                a={a}
+                b={b}
+                specs={specs}
+                options={options}
+                key={JSON.stringify([specs, options])}
+            />
+        </>
+    );
 };
 
 export default DiffPage;
