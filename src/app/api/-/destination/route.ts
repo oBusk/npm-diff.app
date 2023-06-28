07e5af952bb8b56241760d8907d02069ce506947
Oscar Busk <oscar.busk@gmail.com>
2023-06-28 12:01:53 +0200

    Add destination api endpoint

---
+++ src/app/api/-/destination/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import destination from "^/lib/destination";
+
+function assureArrayLength(specs: string[]) {
+    if (specs.length < 3) {
+        return specs as [string] | [string, string];
+    }
+    throw new Error("Too many specs");
+}
+
+export async function GET(req: NextRequest, { params }: { params: {} }) {
+    const { searchParams } = new URL(req.url);
+
+    const specsOrVersions = assureArrayLength(
+        searchParams.getAll("specs") ?? "",
+    );
+
+    console.log("[api/-/destination] running", specsOrVersions);
+
+    const result = await destination(specsOrVersions);
+
+    return NextResponse.json(result);
+}
