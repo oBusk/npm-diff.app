b1fba43e5193262ab130af966260a4236d546263
Oscar Busk <oscar.busk@gmail.com>
2023-11-04 03:21:17 +0100

    Fix tests by mocking unstable_cache

--- jest.setup.js
+++ jest.setup.js
@@ -6,3 +6,10 @@ jest.mock("react", () => {
     React.cache = (fn) => fn;
     return React;
 });
+
+// Override untable_cache method to avoid caching in tests
+jest.mock("next/cache", () => {
+    const cache = jest.requireActual("next/cache");
+    cache.unstable_cache = (fn) => fn;
+    return cache;
+});
