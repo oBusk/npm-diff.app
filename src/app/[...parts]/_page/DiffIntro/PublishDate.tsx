9fc81c2c4dafd95d9c295723c102be7cfd77464c
Oscar Busk <oscar.busk@gmail.com>
2024-03-09 15:34:18 +0100

    Show time on hover

--- src/app/[...parts]/_page/DiffIntro/PublishDate.tsx
+++ src/app/[...parts]/_page/DiffIntro/PublishDate.tsx
@@ -1,5 +1,6 @@
 import { type ComponentProps } from "react";
 import Skeleton from "^/components/ui/Skeleton";
+import Tooltip from "^/components/ui/Tooltip";
 import getVersionData from "^/lib/api/npm/getVersionData";
 import { cx } from "^/lib/cva";
 import type SimplePackageSpec from "^/lib/SimplePackageSpec";
@@ -9,7 +10,7 @@ export interface PublishDateProps extends ComponentProps<"div"> {
     pkg: SimplePackageSpec;
 }
 
-const shared = cx("flex h-5 items-center justify-center");
+const shared = cx("my-1 flex h-5 items-center justify-center");
 
 async function PublishDate({ pkg, className, ...props }: PublishDateProps) {
     const versionData = await getVersionData(pkg);
@@ -17,9 +18,11 @@ async function PublishDate({ pkg, className, ...props }: PublishDateProps) {
     const time = versionData[pkg.version]?.time ?? null;
 
     return (
-        <div className={cx(shared, className)} {...props}>
-            {new Date(time).toLocaleDateString()}
-        </div>
+        <Tooltip label={time ? new Date(time).toLocaleString() : "Unknown"}>
+            <div className={cx(shared, "cursor-help", className)} {...props}>
+                {new Date(time).toLocaleDateString()}
+            </div>
+        </Tooltip>
     );
 }
 
