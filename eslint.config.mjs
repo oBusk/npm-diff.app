69be188c845d1656c5f58ecb1665292a5163b099
Oscar Busk <oscar.busk@gmail.com>
2025-12-15 12:51:21 +0100

    Update config to v16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,5 @@
 import nextObusk from "@obusk/eslint-config-next";
 
-const eslintConfig = [
-    ...nextObusk,
-    {
-        rules: {
-            "@typescript-eslint/ban-ts-comment": 0,
-            "@typescript-eslint/no-explicit-any": 0,
-            "@typescript-eslint/no-require-imports": 0,
-            "@typescript-eslint/no-unused-expressions": 0,
-            "@typescript-eslint/no-unused-vars": 0,
-            "no-var": 0,
-            "prefer-const": 0,
-        },
-    },
-];
+const eslintConfig = [...nextObusk];
 
 export default eslintConfig;
