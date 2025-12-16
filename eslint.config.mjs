bd46734f62ccf8a7ec9424891e9be6dee6580aec
Oscar Busk <oscar.busk@gmail.com>
2025-12-16 23:59:39 +0100

    Disable all rules that causes lint errors, to be enabled 1-by-1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,23 @@
 import nextObusk from "@obusk/eslint-config-next";
 
-const eslintConfig = [...nextObusk];
+const eslintConfig = [
+    ...nextObusk,
+    {
+        name: "Temporary Overrides",
+        // Temporarily disable all failing rules to be fixed 1 by 1
+        rules: {
+            "react-hooks/immutability": "off",
+            "prefer-const": "off",
+            "react-hooks/set-state-in-effect": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "react-hooks/refs": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "no-var": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/no-unused-expressions": "off",
+            "@typescript-eslint/no-require-imports": "off",
+        },
+    },
+];
 
 export default eslintConfig;
