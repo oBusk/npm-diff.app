3f1fef8da020c3b16226e13edfefc8296e5e0817
Oscar Busk <oscar.busk@gmail.com>
2025-12-17 01:12:22 +0100

    Import animate plugin using import to avoid require()

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 export default {
     darkMode: ["class"],
@@ -72,5 +73,5 @@ export default {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [animate],
 } satisfies Config;
