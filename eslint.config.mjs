3f43fea0c59fbe32dbce7351e766f7969b6bf93b
Oscar Busk <oscar.busk@gmail.com>
2025-12-17 01:47:39 +0100

    Remove temporary overrides

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,5 @@
 import nextObusk from "@obusk/eslint-config-next";
 
-const eslintConfig = [
-    ...nextObusk,
-    {
-        name: "Temporary Overrides",
-        // Temporarily disable all failing rules to be fixed 1 by 1
-        rules: {},
-    },
-];
+const eslintConfig = [...nextObusk];
 
 export default eslintConfig;
