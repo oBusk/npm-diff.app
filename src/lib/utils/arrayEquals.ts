14a95b13624a27ac3b171e67fbacea71b5acf20a
Oscar Busk <oscar.busk@gmail.com>
2025-12-17 01:01:30 +0100

    Enable and fix all finds of `no-var`

--- src/lib/utils/arrayEquals.ts
+++ src/lib/utils/arrayEquals.ts
@@ -8,7 +8,7 @@ function arrayEquals(a: unknown[], b: unknown[]) {
     if (a.length !== b.length) {
         return false;
     }
-    for (var i = 0; i < a.length; ++i) {
+    for (let i = 0; i < a.length; ++i) {
         if (a[i] !== b[i]) {
             return false;
         }
