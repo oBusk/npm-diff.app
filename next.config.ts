e325c2c27e27c000cfa0fcd07b42bd19ec28228d
Oscar Busk <oscar.busk@gmail.com>
2026-01-07 16:45:51 +0100

    Enable reactCompiler
    https://nextjs.org/docs/app/api-reference/config/next-config-js/reactCompiler

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+    reactCompiler: true,
     serverExternalPackages: ["libnpmdiff", "npm-package-arg", "pacote"],
 };
 
