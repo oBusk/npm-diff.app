6f32d33711677ab0a3f40162507dfe779dcf2c82
Oscar Busk <oscar.busk@gmail.com>
2026-01-16 21:39:29 +0100

    Update examples

--- src/lib/examples.ts
+++ src/lib/examples.ts
@@ -1,8 +1,8 @@
 const EXAMPLES = Object.freeze([
-    "@types/node@15.12.1...@types/node@15.12.2",
-    "lodash@4.17.0...4.17.19",
-    "4.0.0...chalk@4.1.0",
-    "tslib@1.13.0...tslib@2.0.0",
+    "@types/node@24.10.8...@types/node@24.10.9",
+    "lodash@4.17.0...4.17.21",
+    "5.0.0...chalk@5.2.0",
+    "tslib@1...tslib@2",
 ] as const);
 
 export default EXAMPLES;
