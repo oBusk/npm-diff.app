2e8260809627fce476a002ee8ac994343ded21c7
Oscar Busk <oscar.busk@gmail.com>
2026-01-16 21:20:50 +0100

    Update fallback autocomplete suggestions

--- src/lib/api/npms/popularPackages.ts
+++ src/lib/api/npms/popularPackages.ts
@@ -1,5 +1,30 @@
 import search from "./search";
 
 export default function popularPackages(size: number) {
-    return search("not:deprecated", size);
+    /**
+     * Using a hardcoded list as npms.io does not really tell us anything about popularity
+     */
+    const hardcodedList = [
+        "react",
+        "@angular/core",
+        "typescript",
+        "chalk",
+        "express",
+        "lodash",
+        "vue",
+        "axios",
+        "jest",
+        "webpack",
+        "moment",
+    ];
+
+    return Promise.all(
+        hardcodedList.slice(0, size).map((v) => search(v, 1)),
+    ).then((results) => {
+        const packages = results.flatMap((result) => result.results);
+        return {
+            total: packages.length,
+            results: packages,
+        };
+    });
 }
